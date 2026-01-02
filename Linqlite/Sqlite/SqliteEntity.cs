c2967494df3406fdb133497cb9a074c278a45a7d
Patorche <patrice.labbe@outlook.fr>
2026-01-02 16:12:53 +0100

    Oublie classe SqlEntity

---
+++ Linqlite/Sqlite/SqliteEntity.cs
@@ -0,0 +1,22 @@
+﻿using Linqlite.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Linqlite.Sqlite
+{
+    public class SqliteEntity : ObservableObject
+    {
+        public bool IsNew = true;
+
+        protected override bool SetProperty<TValue>(ref TValue storage, TValue value, [CallerMemberName] string propertyName = "")
+        {
+            bool changed = base.SetProperty(ref storage, value, propertyName);
+           return changed;
+        }
+
+    }
+
+}

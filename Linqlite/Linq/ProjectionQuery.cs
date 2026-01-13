9ec4bb4a12774951cdd696a996f7191086c5f087
Patorche <patrice.labbe@outlook.fr>
2026-01-13 11:30:17 +0100

    Ajout projections Dto et anonymes (Tuples à venir)

---
+++ Linqlite/Linq/ProjectionQuery.cs
@@ -0,0 +1,57 @@
+﻿using Linqlite.Hydration;
+using Linqlite.Linq.SqlExpressions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Text;
+
+namespace Linqlite.Linq
+{
+    internal class ProjectionQuery<T>
+    {
+        public static List<T?> Execute(string sql, LinqliteProvider provider, IReadOnlyDictionary<string, object> parameters, SqlMemberProjectionExpression infos)
+        {
+            if (provider.Connection == null) throw new ArgumentNullException("Aucune connection n'est active pour le Provider");
+            using var cmd = provider.Connection.CreateCommand();// sql, parameters);
+            cmd.CommandText = sql;
+            using var reader = cmd.ExecuteReader();
+
+            var list = new List<T?>();
+
+            while (reader.Read())
+            {
+                T? e = default;
+                if(IsAnonymousType(typeof(T)))
+                    e = HydratorBuilder.HydrateAnonymous<T>(reader);
+                else if (IsBaseType(typeof(T))){
+                    e = (T)reader.GetValue(0);
+                }
+                else if (IsDto(typeof(T)))
+                {
+                    e = HydratorBuilder.HydrateDto<T>(reader, infos);
+                }
+
+                    list.Add(e);
+            }
+
+            return list;
+        }
+
+        private static bool IsAnonymousType(Type t) => t.Name.Contains("AnonymousType") && t.IsSealed && t.IsNotPublic;
+        private static bool IsBaseType(Type t)
+        {
+            t = Nullable.GetUnderlyingType(t) ?? t; 
+                return t.IsPrimitive || 
+                       t.IsEnum || 
+                       t == typeof(string) || 
+                       t == typeof(decimal) || 
+                       t == typeof(DateTime) || 
+                       t == typeof(Guid) || 
+                       t == typeof(byte[]);
+        }
+
+        private static bool IsDto(Type t) => !IsBaseType(t) && !IsAnonymousType(t);
+
+    }
+}
--- Linqlite/Linq/SqlVisitor/AbstractSourceHandler.cs
+++ Linqlite/Linq/SqlVisitor/AbstractSourceHandler.cs
@@ -11,22 +11,51 @@ namespace Linqlite.Linq.SqlVisitor
 {
     internal abstract class AbstractSourceHandler
     {
-        protected AbstractSqlProjectionExpression HandleProjection(Expression body, SqlTreeBuilderVisitor builder) 
+        protected AbstractSqlProjectionExpression HandleProjection(Expression body, SqlTreeBuilderVisitor builder, SqlSelectExpression select) 
         { 
             switch (body)
             {
                 case MemberExpression member:
-                    return HandleMemberProjection(member, builder);
+                    return HandleMemberProjection(member, builder, select);
 
                 case NewExpression nex:
-                    return HandleNewProjection(nex, builder);
+                    return HandleNewProjection(nex, builder, select);
+                
+                case MemberInitExpression mie:
+                    return HandleInitProjection(mie, builder, select);
 
                 default:
                     throw new NotSupportedException($"Expression select non supportée: {body.NodeType}");
             }
         }
 
-        private AbstractSqlProjectionExpression HandleMemberProjection(MemberExpression member, SqlTreeBuilderVisitor builder)
+        private SqlMemberProjectionExpression HandleInitProjection(MemberInitExpression init, SqlTreeBuilderVisitor builder, SqlSelectExpression select)
+        {
+            if (init == null)
+                throw new UnreachableException("Tentative d'ajout d'une projection null !");
+
+            var members = new Dictionary<MemberInfo, SqlExpression>();
+
+            foreach (var binding in init.Bindings)
+            {
+                if (binding is MemberAssignment assign)
+                {
+                    var sqlExpr = builder.Visit(assign.Expression) as SqlExpression;
+                    if (sqlExpr == null)
+                        throw new UnreachableException("Tentative d'ajout d'une projection null !");
+                    members.Add(assign.Member, sqlExpr);
+                }
+                else
+                {
+                    throw new NotSupportedException($"Binding non supporté : {binding.BindingType}");
+                }
+            }
+
+            return new SqlMemberProjectionExpression(members, init.Type);
+        }
+
+
+        private AbstractSqlProjectionExpression HandleMemberProjection(MemberExpression member, SqlTreeBuilderVisitor builder, SqlSelectExpression select)
         {
             var sqlExpr = (SqlExpression)builder.Visit(member);
             if(sqlExpr is SqlEntityReferenceExpression er)
@@ -63,7 +92,7 @@ namespace Linqlite.Linq.SqlVisitor
             return members;
         }
 
-        private SqlMemberProjectionExpression HandleNewProjection(NewExpression nex, SqlTreeBuilderVisitor builder)
+        private SqlMemberProjectionExpression HandleNewProjection(NewExpression nex, SqlTreeBuilderVisitor builder, SqlSelectExpression select)
         {
             if (nex == null)
                 throw new UnreachableException("Tentative d'ajout d'une projection null !");
@@ -76,6 +105,7 @@ namespace Linqlite.Linq.SqlVisitor
                 var member = nex.Members?[i];
                 var argument = nex.Arguments[i];
                 var sqlExpr = builder.Visit(argument) as SqlExpression;
+
                 if (member == null || sqlExpr == null)
                     throw new UnreachableException("Tentative d'ajout d'une projection null !");
                 members.Add(member, sqlExpr);
@@ -83,6 +113,29 @@ namespace Linqlite.Linq.SqlVisitor
 
             return new SqlMemberProjectionExpression(members, nex.Type);
         }
+
+     /*   private SqlMemberProjectionExpression HandleCallProjection(MethodCallExpression call, SqlTreeBuilderVisitor builder)
+        {
+            var members = new Dictionary<MemberInfo, SqlExpression>();
+
+            // On récupère les paramètres de la méthode
+            var parameters = call.Method.GetParameters();
+
+            for (int i = 0; i < call.Arguments.Count; i++)
+            {
+                var arg = call.Arguments[i];
+                var sqlExpr = builder.Visit(arg) as SqlExpression;
+                if (sqlExpr == null)
+                    throw new UnreachableException("Projection null dans un appel de méthode");
+
+                var member = parameters[i];
+                members.Add(member, sqlExpr);
+            }
+
+            return new SqlMemberProjectionExpression(members, call.Type);
+        }
+     */
+
 
     }
 }
